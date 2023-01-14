ccb389204daa8bba1f7759bb4418f62a3895eb00
Samuel Holmes <sam@edge.app>
2023-01-14 01:17:54 -0800

    Add leet server to generator

--- server/generate.ts
+++ server/generate.ts
@@ -1,3 +1,4 @@
+import express from "express"
 import { config } from 'dotenv'
 import { ChatGPTAPIBrowser } from 'chatgpt'
 import { readFileSync } from 'fs'
@@ -12,30 +13,60 @@ const OPENAI_PASSWORD = process.env.OPENAI_PASSWORD
 const wordText = readFileSync('./words.txt', 'utf-8')
 const words = wordText.split('\n')
 
+const app = express();
+const cors = require("cors");
+const port = 1337;
+
+let api: ChatGPTAPIBrowser | undefined
+let ready = false
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/phrase', (req, res) => {
+  if (api == null || !ready) return res.status(500).send('Waiting on chappie...')
+
+  generate(api).then(phrase => {
+    console.log(`Request generated: ${phrase}`)
+    res.send({ phrase })
+  }).catch(err => {
+    res.status(500).send(err)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
 
 async function main() {
   if (OPENAI_EMAIL == null || OPENAI_PASSWORD == null) throw new Error('missing un/pw')
 
-  const api = new ChatGPTAPIBrowser({
+  api = new ChatGPTAPIBrowser({
       email: OPENAI_EMAIL,
       password: OPENAI_PASSWORD 
     })
 
-  await api.initSession()
+    
+    await api.initSession()
 
+  ready = true
+  
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   })
-
+  
   rl.on('line', async () => {
-    await generate(api)
+    if (api == null)  throw new Error('unexpected null api')
+    const phrase = await generate(api)
+    console.log(`\n\t${phrase}`)
   })
+
 }
 
 main().catch(console.error)
 
-async function generate(api: ChatGPTAPIBrowser) {
+async function generate(api: ChatGPTAPIBrowser): Promise<string> {
 
   const mnemonic = generateMnemonic(words)
   const five = mnemonic.split(' ').slice(0, 5).join(', ')
@@ -46,10 +77,10 @@ async function generate(api: ChatGPTAPIBrowser) {
 
   // console.log(prompt)
 
-  const respond = async (msg: string): Promise<void> => {
+  const respond = async (msg: string): Promise<string> => {
     const result = await api.sendMessage(msg)
-    console.log(`\n\t${result.response}`)
+    return result.response
   }
 
-  respond(prompt)
+  return await respond(prompt)
 }
