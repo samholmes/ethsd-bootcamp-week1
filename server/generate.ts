d633faef2a93c4307e3e4e7536b7e8b7c874a297
Samuel Holmes <sam@edge.app>
2023-01-14 18:31:31 -0800

    Make adjustments to the generator AI prompts

--- server/generate.ts
+++ server/generate.ts
@@ -11,7 +11,7 @@ const OPENAI_EMAIL = process.env.OPENAI_EMAIL
 const OPENAI_PASSWORD = process.env.OPENAI_PASSWORD
 
 const wordText = readFileSync('./words.txt', 'utf-8')
-const words = wordText.split('\n')
+const wordList = wordText.split('\n')
 
 const app = express();
 const cors = require("cors");
@@ -26,8 +26,12 @@ app.use(express.json());
 app.use('/phrase', (req, res) => {
   if (api == null || !ready) return res.status(500).send('Waiting on chappie...')
 
+  const header = `REST request`
+  console.log(header)
+  console.log(Array.from(header).map(() => '-').join(''))
   generate(api).then(phrase => {
-    console.log(`Request generated: ${phrase}`)
+    console.log(phrase)
+    console.log(Array.from(header).map(() => '-').join(''))
     res.send({ phrase })
   }).catch(err => {
     res.status(500).send(err)
@@ -35,7 +39,7 @@ app.use('/phrase', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
+  console.log(`Listening on port ${port}`);
 });
 
 async function main() {
@@ -50,6 +54,8 @@ async function main() {
     await api.initSession()
 
   ready = true
+
+  console.log(`Ready for sweet sweet mnemonics!`)
   
   const rl = readline.createInterface({
     input: process.stdin,
@@ -59,7 +65,6 @@ async function main() {
   rl.on('line', async () => {
     if (api == null)  throw new Error('unexpected null api')
     const phrase = await generate(api)
-    console.log(`\n\t${phrase}`)
   })
 
 }
@@ -68,19 +73,31 @@ main().catch(console.error)
 
 async function generate(api: ChatGPTAPIBrowser): Promise<string> {
 
-  const mnemonic = generateMnemonic(words)
-  const five = mnemonic.split(' ').slice(0, 5).join(', ')
+  const mnemonic = generateMnemonic(wordList)
+  const word = mnemonic.split(' ')
+  const five = word.slice(0, 5).join(', ')
 
-  console.log(`Using words: ${five}`)
 
-  const prompt = `Write a short sentence using 5 of the words in this list: ${five}. The sentence must be no more than 9 words long. The sentences should require no punctuation. The sentence should be coherent and make sense.Add filler words to the sentence like an, the, a, etc. And, add unique, random, and distinct adjectives to some nouns.`
+  const header = `Using words: [${five}]`
+  console.log(header)
+  console.log(Array.from(header).map(() => '-').join(''))
 
   // console.log(prompt)
 
-  const respond = async (msg: string): Promise<string> => {
+  const chat = async (msg: string): Promise<string> => {
     const result = await api.sendMessage(msg)
+    console.log('\t' + result.response)
     return result.response
   }
 
-  return await respond(prompt)
+  const final = await chat(`Write a short sentence using all of the words in this list: ${five}. `
+    +`The sentence must be exactly 12 words long. `
+    +`The sentences so that no punctuation is used. `
+    +`The sentence to be coherent and make sense. `
+    +`The sentence to include determiner words. `
+    +`Add one or more adjective the nouns in the sentence that are not included in the BIP39 word list.`)
+
+  process.stdout.write(Array.from(header).map(() => '-').join(''))
+
+  return final
 }
